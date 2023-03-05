1976cac41fbaeb249179360ef85e99d76500156c
aaron <zhaojiewang219@yeah.net>
2023-03-05 17:54:24 +0800

    first commit

---
+++ autostart.sh
@@ -0,0 +1,8 @@
+slstatus &
+
+/bin/bash ~/.dwm/wallhaven/autotoggle.sh &
+
+fcitx5 &
+
+sleep 3
+picom --backend glx &
---
+++ wallhaven/autotoggle.sh
@@ -0,0 +1,5 @@
+while true; do
+#	bash ~/.dwm/wallhaven/setup.s
+  feh --bg-fill --randomize --no-fehbg ~/wallhaven/wallhaven-kx8qmq_1920x1080.png
+	sleep 5m
+done
